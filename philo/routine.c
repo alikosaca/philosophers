45d3c425c3cb92f5edfa34b457e6016b58eb4a81
alikosaca <alikosaca0@gmail.com>
2025-08-20 16:18:33 +0300

    fix(simulation): resolve death timing issues in eat/dream operations

--- philo/routine.c
+++ philo/routine.c
@@ -6,26 +6,11 @@
 /*   By: akosaca <akosaca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 17:55:12 by akosaca           #+#    #+#             */
-/*   Updated: 2025/08/15 02:30:25 by akosaca          ###   ########.fr       */
+/*   Updated: 2025/08/20 15:25:32 by akosaca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-
-void	think(t_philosopher *philo)
-{
-	print_message("is thinking", philo);
-	return ;
-}
-
-void	dream(t_philosopher *philo)
-{
-	if (check_simulation_end(philo->simulation))
-		return ;
-	print_message("is sleeping", philo);
-	ft_usleep(philo->simulation->time_to_sleep, philo->simulation);
-}
 
 void	is_one_philo(t_philosopher *philo)
 {
@@ -41,8 +26,25 @@ void	is_one_philo(t_philosopher *philo)
 	}
 }
 
-// if (check_simulation_end(philo->simulation))//eklendi
-// 	return ;
+void	think(t_philosopher *philo)
+{
+	print_message("is thinking", philo);
+	if (philo->simulation->num_philo % 2 == 1
+		&& philo->simulation->time_to_eat == philo->simulation->time_to_sleep
+		&& (philo->id % 2 == 1))
+		ft_usleep(1, philo->simulation);
+	return ;
+}
+
+void	dream(t_philosopher *philo)
+{
+	if (check_simulation_end(philo->simulation))
+		return ;
+	print_message("is sleeping", philo);
+	ft_usleep(philo->simulation->time_to_sleep, philo->simulation);
+}
+
+
 void eat(t_philosopher *philo)
 {
 	pthread_mutex_t	*first_fork;
@@ -71,72 +73,3 @@ void eat(t_philosopher *philo)
 	pthread_mutex_unlock(first_fork);
 	pthread_mutex_unlock(second_fork);
 }
-
-	// if (check_simulation_end(philo->simulation))//eklendi
-	// {
-	// 	pthread_mutex_unlock(first_fork);
-	// 	return;
-	// }
-
-
-	// if (philo->simulation->number_of_meals > 0 && is_enough(philo))//eklendi 72. satır
-    // {
-    //     pthread_mutex_unlock(first_fork);
-    //     pthread_mutex_unlock(second_fork);
-    //     return;
-    // }
-
-
-
-
-
-
-
-	// if (philo->left_fork == philo->right_fork) //! tek filo gelirse, başka fonksiyona ayır
-	// {
-	// 	pthread_mutex_lock(&philo->left_fork->mutex);
-	// 	print_message("has taken a fork", philo);
-	// 	ft_usleep(philo->simulation->time_to_die, philo->simulation);
-	// 	pthread_mutex_unlock(&philo->left_fork->mutex);
-	// 	philo->simulation->is_dead = true;
-	// 	if (check_simulation_end(philo->simulation))
-	// 		return ;
-	// }
-
-
-
-
-
-
-
-
-// void	eat(t_philosopher *philo) 
-// {
-// 	if (check_simulation_end(philo))
-// 		return (1);
-// 	printf("id: %d\n", philo->id);
-// 	if (philo->id % 2 == 0)
-// 	{
-// 		pthread_mutex_lock(&philo->left_fork->mutex);
-// 		print_message("has taken a fork left", philo);
-// 		pthread_mutex_lock(&philo->right_fork->mutex);
-// 		print_message("has taken a fork right", philo);
-// 	}
-// 	else
-// 	{
-// 		pthread_mutex_lock(&philo->right_fork->mutex);
-// 		print_message("has taken a fork right", philo);
-// 		pthread_mutex_lock(&philo->left_fork->mutex);
-// 		print_message("has taken a fork left", philo);
-// 	}
-// 	print_message("is eating", philo);
-// 	//philo->is_eating = true; //?
-// 	pthread_mutex_lock(philo->simulation->meal_mutex);
-// 	philo->last_meal_time = get_current_time();
-// 	philo->eat_count++;
-// 	pthread_mutex_unlock(philo->simulation->meal_mutex);
-// 	ft_usleep(philo->simulation->time_to_eat, philo->simulation);
-// 	//philo->is_eating = false;
-// 	pthread_mutex_unlock(&philo->left_fork->mutex);
-// 	pthread_mutex_unlock(&philo->right_fork->mutex);
-// }
