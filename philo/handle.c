6c6045f409b52d08a7d13ccb40d15c5715686e2b
alikosaca <alikosaca0@gmail.com>
2025-08-20 16:14:11 +0300

    fix(simulation): add death check before print_message operation

--- philo/handle.c
+++ philo/handle.c
@@ -6,7 +6,7 @@
 /*   By: akosaca <akosaca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 12:38:02 by akosaca           #+#    #+#             */
-/*   Updated: 2025/08/15 02:19:47 by akosaca          ###   ########.fr       */
+/*   Updated: 2025/08/17 17:16:24 by akosaca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,10 @@ void	print_message(char *str, t_philosopher *philo)
 {
 	long long	time;
 
-	pthread_mutex_lock(philo->simulation->dead_lock);
-	if (philo->simulation->is_dead)
-	{
-		pthread_mutex_unlock(philo->simulation->dead_lock);
+	if (check_simulation_end(philo->simulation))
 		return ;
-	}
 	pthread_mutex_lock(philo->simulation->write_mutex);
 	time = get_current_time() - philo->simulation->start_time;
 	printf("%lld %d %s\n", time, philo->id, str);
 	pthread_mutex_unlock(philo->simulation->write_mutex);
-	pthread_mutex_unlock(philo->simulation->dead_lock);
 }
